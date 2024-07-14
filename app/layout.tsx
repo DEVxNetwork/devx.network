233f4baa048c533db17f48727ad921caaa9790ef
AJ Caldwell <tomatrow@gmail.com>
2024-07-14 09:38:32 -0700

    prettier

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,20 @@
-import { ReactNode } from "react";
-import type { Metadata } from "next";
-import "./globals.css";
+import { ReactNode } from "react"
+import type { Metadata } from "next"
+import "./globals.css"
 
 export const metadata: Metadata = {
-  title: "San Diego DEVx",
-  description: "All things Software Development",
-};
+	title: "San Diego DEVx",
+	description: "All things Software Development"
+}
 
 export default function RootLayout({
-  children,
+	children
 }: Readonly<{
-  children: ReactNode;
+	children: ReactNode
 }>) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body>{children}</body>
+		</html>
+	)
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,3 @@
 export default function Home() {
-  return (
-    <main>
-       SD DEVx
-    </main>
-  );
+	return <main>SD DEVx</main>
 }
