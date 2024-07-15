0c090408eed4441dcda23c06710345078464f2e8
Ezekiel <engineeringgeek3.14@gmail.com>
2024-07-15 02:02:23 -0700

    Beginning work on 'Talk' page route, with functional sqlite integration. Potentially needs captcha integration, admin panel, submission page, and styling for displaying existing talks

--- app/talk/page.tsx
+++ app/talk/page.tsx
@@ -1,3 +1,18 @@
-export default function Talk() {
-	return <main>/talk</main>
+import { getTalks } from "../api/talk-requests/route";
+import "./page.css";
+
+export default async function Talk() {
+	const data = await getTalks();
+	return (
+		<main>
+			Current talks: {JSON.stringify(data)} <br />
+			<form className="max-w-screen-md m-auto" action="/api/talk-requests" method="POST">
+				<input type="text" placeholder="Speaker Name" name="speaker" />
+				<input type="text" placeholder="Talk Title" name="title" />
+				<textarea placeholder="Talk Description..." name="description"></textarea>
+				<input className="btn hover:bg-accent hover:text-base-100" type="reset" value="Reset" />
+				<button className="btn hover:bg-accent hover:text-base-100">Submit</button>
+			</form>
+		</main>
+	)
 }
