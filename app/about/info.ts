d0ceefe6077b5dffbf80a34dd13b8e78cfe71bc2
Tim <tpadjen@gmail.com>
2024-07-18 12:56:08 -0700

    Refactor organizer cards into separate component for reusability

---
+++ app/about/info.ts
@@ -0,0 +1,22 @@
+export const organizers = [
+	{
+		name: "Sam Holmes",
+		imageSrc: "/images/sam-about.jpeg",
+		linkedIn: "https://www.linkedin.com/in/sammyholmes/"
+	},
+	{
+		name: "Tryston Perry",
+		imageSrc: "/images/tryston-about.jpeg",
+		linkedIn: "https://www.linkedin.com/in/trystonperry/"
+	},
+	{
+		name: "AJ Caldwell",
+		imageSrc: "/images/aj-about.jpeg",
+		linkedIn: "https://www.linkedin.com/in/aj-caldwell/"
+	},
+	{
+		name: "Zane Prunella",
+		imageSrc: "/images/zane-about.jpeg",
+		linkedIn: "https://www.linkedin.com/in/zprunella/"
+	}
+]
