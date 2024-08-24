67827615911b9e2ffa5dcc2aaf6f9238adbbab32
Sam Holmes <sam@edge.app>
2024-08-23 20:25:49 -0700

    Redesigned homepage with large animated hero

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -22,7 +22,7 @@ const Header = () => {
 						</div>
 						<ul
 							tabIndex={0}
-							className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+							className="menu menu-lg dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
 						>
 							<NavLinks />
 						</ul>
--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,31 @@
 import React from "react"
+import { PotionBackground } from "./components/PotionBackground"
 
 const Home: React.FC = () => {
 	return (
-		<main className="p-8 bg-gray-900 min-h-screen text-white">
-			<section className="relative mb-12">
-				<img
-					src="/images/logo.jpeg"
-					alt="Developer meetup hero"
-					className="w-full h-[50rem] object-cover rounded-lg shadow-md opacity-75"
-				/>
-			</section>
+		<>
+			<PotionBackground />
+			<main className="p-8 min-h-screen text-white flex flex-col items-center justify-center">
+				<section className="relative">
+					<img
+						src="/images/sd-devx-brand.png"
+						alt="Developer meetup hero"
+						style={{
+							width: "100%",
+							maxWidth: "512px",
+							margin: "0 auto"
+						}}
+					/>
+				</section>
 
-			<section className="bg-gray-800 p-8 rounded-lg shadow-md mb-12">
-				<h2 className="text-3xl font-bold mb-4 text-center">Join us</h2>
-				<div className="w-full flex justify-center">
-					<p className="mt-2 text-xl text-center max-w-screen-lg">
-						Fostering developer community through meetups and open-source projects in California.
+				<section className="p-12 rounded-lg mb-12">
+					<p className="text-xl text-center max-w-screen-lg">
+						Fostering developer community through events and open-source projects in San Diego,
+						California.
 					</p>
-				</div>
-			</section>
+				</section>
 
-			{/* <section className="bg-gray-800 p-8 rounded-lg shadow-md mb-12">
+				{/* <section className="bg-gray-800 p-8 rounded-lg shadow-md mb-12">
 				<h2 className="text-3xl font-bold mb-4 text-center">Past Events</h2>
 				<div className="w-full flex justify-center">
 					<p className="mt-2 text-xl text-center max-w-screen-lg">
@@ -30,16 +35,17 @@ const Home: React.FC = () => {
 				</div>
 			</section> */}
 
-			<section className="bg-gray-800 p-8 rounded-lg shadow-md mb-12">
-				<h2 className="text-3xl font-bold mb-4 text-center">Sponsors</h2>
-				<div className="w-full flex justify-center">
-					<p className="mt-2 text-xl text-center max-w-screen-lg">
-						We are grateful for our sponsors who make our events possible. Interested in sponsoring?
-						Reach out to us on Discord/Meetup or at the event.
-					</p>
-				</div>
-			</section>
-		</main>
+				{/* <section className="bg-gray-800 p-8 rounded-lg shadow-md mb-12">
+					<h2 className="text-3xl font-bold mb-4 text-center">Sponsors</h2>
+					<div className="w-full flex justify-center">
+						<p className="mt-2 text-xl text-center max-w-screen-lg">
+							We are grateful for our sponsors who make our events possible. Interested in
+							sponsoring? Reach out to us on Discord/Meetup or at the event.
+						</p>
+					</div>
+				</section> */}
+			</main>
+		</>
 	)
 }
 
