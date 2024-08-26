d13a1fc05f3ac5cf72ebd565c18790e7aed26668
Sam Holmes <sam@edge.app>
2024-08-25 20:41:01 -0700

    Add open-graph metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,14 @@ import Footer from "./components/Footer"
 
 export const metadata: Metadata = {
 	title: "San Diego DEVx",
-	description: "All things Software Development"
+	description: "Fostering developer community through events and open-source projects.",
+	openGraph: {
+		type: "website",
+		url: "https://devx.network",
+		title: "San Diego DEVx",
+		description: "Fostering developer community through events and open-source projects.",
+		images: "/images/DEVxCover.png"
+	}
 }
 
 export default function RootLayout({
