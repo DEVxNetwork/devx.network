baba0d7e705ea14b300d354f737dd4e76856a505
AJ Caldwell <tomatrow@gmail.com>
2024-10-09 09:20:46 -0700

    wip

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -52,6 +52,54 @@ const Anchor = styled.a<{
 	}
 `
 
+const Button = styled.a<{
+	ghost?: boolean
+}>`
+	display: inline-flex;
+	justify-content: center;
+	align-items: center;
+
+	font-size: 0.875rem;
+	text-align: center;
+	border-radius: 0.5rem;
+	transition: background 200ms ease-in-out;
+
+	color: #a7adba;
+
+	height: 3rem;
+	padding: 0 1rem;
+	font-weight: 600;
+	line-height: 1em;
+
+	${(props) => {
+		const backgroundColor = props.ghost ? "transparent" : "#1a1e23"
+
+		return css`
+			background: ${backgroundColor};
+			border: 1px solid ${backgroundColor};
+		`
+	}}
+
+	&:hover {
+		${(props) => {
+			if (props.ghost)
+				return css`
+					background: color-mix(in srgb, white 5%, transparent 100%);
+					border: 1px solid transparent;
+				`
+			else
+				return css`
+					background: #15181d;
+					border: 1px solid #15181d;
+				`
+		}}
+	}
+`
+
+const ImageAnchor = styled(Anchor)`
+	padding: 0;
+`
+
 const NavLink = styled.a`
 	display: inline-flex;
 	justify-content: center;
@@ -76,13 +124,19 @@ const NavLink = styled.a`
 	}
 `
 
+const DropdownButton = styled(Button)`
+	@media (min-width: 1024px) {
+		display: none;
+	}
+`
+
 const Header = () => {
 	return (
 		<header>
 			<nav className="navbar bg-base-100">
 				<div className="navbar-start">
 					<div className="dropdown">
-						<div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+						<DropdownButton tabIndex={0} ghost>
 							<svg
 								xmlns="http://www.w3.org/2000/svg"
 								className="w-5 h-5"
@@ -97,7 +151,7 @@ const Header = () => {
 									d="M4 6h16M4 12h8m-8 6h16"
 								/>
 							</svg>
-						</div>
+						</DropdownButton>
 						<ul
 							tabIndex={0}
 							className="menu menu-lg dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
@@ -105,9 +159,9 @@ const Header = () => {
 							<NavLinks />
 						</ul>
 					</div>
-					<a className="inline p-0 btn btn-ghost" href="/">
+					<ImageAnchor ghost href="/">
 						<LogoImage src="/images/logo.jpeg" />
-					</a>
+					</ImageAnchor>
 				</div>
 				<div className="hidden navbar-center lg:flex">
 					<ul className="px-1 menu menu-horizontal">
