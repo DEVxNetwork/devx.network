50a4b32a0ac9faf51ddd8e75e82d051ad08d6c37
AJ Caldwell <tomatrow@gmail.com>
2024-10-08 21:56:20 -0700

    Style an anchor button

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -8,6 +8,34 @@ const LogoImage = styled.img`
 	width: 3rem;
 `
 
+const Anchor = styled.a`
+	display: inline-flex;
+	justifiy-content: center;
+	align-items: center;
+	color: #a7adba;
+	background: #1a1e23;
+	border: 1px solid #1a1e23;
+	height: 3rem;
+	border-radius: 0.5rem;
+	padding: 0 1rem;
+	text-align: center;
+	font-size: 0.875rem;
+	line-height: 1em;
+	font-weight: 600;
+	transition:
+		color 0.2s cubic-bezier(0, 0, 0.2, 1) 0s,
+		background-color 0s,
+		border-color 0s,
+		opacity 0s,
+		box-shadow 0s,
+		transform 0s;
+
+	&:hover {
+		background: #15181d;
+		border: 1px solid #15181d;
+	}
+`
+
 const Header = () => {
 	return (
 		<header>
@@ -47,9 +75,7 @@ const Header = () => {
 					</ul>
 				</div>
 				<div className="navbar-end">
-					<a className="btn" href={links.discord}>
-						Join Us on Discord
-					</a>
+					<Anchor href={links.discord}>Join Us on Discord</Anchor>
 				</div>
 			</nav>
 		</header>
