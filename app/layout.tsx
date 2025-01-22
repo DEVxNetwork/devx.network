d13b06ebc3290c87cb38a8f843f87e29bf222ef7
Sam Holmes <sam@edge.app>
2025-01-22 08:26:40 -0800

    Add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,9 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
+			<head>
+				<link rel="icon" href="/images/favicon.png" />
+			</head>
 			<body>
 				<Header />
 				{children}
