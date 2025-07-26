132941e9a2e19e02d281aa684fb8ec29861fb405
Ezekiel Lopez <ez@ezez.win>
2025-07-25 23:16:53 -0700

    Add pnpm lockfile and create layout for Talk page
    - Introduced `pnpm-lock.yaml` to manage package dependencies.
    - Created `layout.tsx` for the Talk page, including metadata and structure with Header and Footer components.
    - Updated `page.css` for consistent styling.
    - Modified `page.tsx` to display a placeholder structure for the Talk page.

---
+++ app/talk/layout.tsx
@@ -0,0 +1,41 @@
+import { ReactNode } from "react"
+import type { Metadata } from "next"
+import "../globals.css"
+import { Header } from "../components/Header"
+import { Footer } from "../components/Footer"
+import { siteConfig } from "../siteConfig"
+import StyledComponentsRegistry from "../StyledComponentsRegistry"
+
+export const metadata: Metadata = {
+	title: siteConfig.title,
+	description: siteConfig.description,
+	openGraph: {
+		type: "website",
+		url: siteConfig.url,
+		title: siteConfig.title,
+		description: siteConfig.description,
+		images: siteConfig.ogImage
+	}
+}
+
+export default function RootLayout({
+	children
+}: Readonly<{
+	children: ReactNode
+}>) {
+	return (
+		<html lang="en">
+			<head>
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<link rel="icon" href="/images/favicon.png" />
+			</head>
+			<body>
+				<StyledComponentsRegistry>
+					<Header />
+					{children}
+					<Footer />
+				</StyledComponentsRegistry>
+			</body>
+		</html>
+	)
+}
