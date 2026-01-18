17f5a340fe08f49a4ccf60d0fc1ff2419d54800c
AJ Caldwell <tomatrow@gmail.com>
2026-01-17 22:51:17 -0800

    Add angel as a current org member, and past org memebers

--- app/info/organizers.ts
+++ app/info/organizers.ts
@@ -7,6 +7,11 @@ export const organizers = [
 		linkedIn: "https://www.linkedin.com/in/aj-caldwell/"
 	},
 	{
+		name: "Angel Vazquez",
+		imageSrc: "/images/profile-photos/angel-2026.png",
+		linkedIn: "https://www.linkedin.com/in/angelthedev/"
+	},
+	{
 		name: "Ezekiel Lopez",
 		imageSrc: "/images/profile-photos/ez-about.png",
 		linkedIn: "https://www.linkedin.com/in/ezekiel-lopez-633b57212/"
@@ -21,9 +26,17 @@ export const organizers = [
 		imageSrc: "/images/profile-photos/sam-2026.png",
 		linkedIn: "https://www.linkedin.com/in/sammyholmes/"
 	}
-	//, {
-	// 	name: "Zane Prunella",
-	// 	imageSrc: "/images/zane-about.webp",
-	// 	linkedIn: "https://www.linkedin.com/in/zprunella/"
-	// }
+]
+
+export const organizerEmeritus = [
+	{
+		name: "Zane Prunella",
+		imageSrc: "/images/profile-photos/zane-2026.png",
+		linkedIn: "https://www.linkedin.com/in/zprunella/"
+	},
+	{
+		name: "Tryston Perry",
+		imageSrc: "/images/profile-photos/tryston-2026.png",
+		linkedIn: "https://www.linkedin.com/in/trystonperry/"
+	}
 ]
