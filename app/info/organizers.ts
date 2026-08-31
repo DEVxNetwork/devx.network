8c3467196120515e59c6bfb3ec5dd63f9cbf687a
AJ Caldwell <tomatrow@gmail.com>
2026-08-30 17:27:29 -0700

    Add cammie and sonia

--- app/info/organizers.ts
+++ app/info/organizers.ts
@@ -12,6 +12,11 @@ export const organizers = [
 		linkedIn: "https://www.linkedin.com/in/angelthedev/"
 	},
 	{
+		name: "Cammie Clay",
+		imageSrc: "/images/profile-photos/cammie-2026.webp",
+		linkedIn: "https://www.linkedin.com/in/cammieclay/"
+	},
+	{
 		name: "Ezekiel Lopez",
 		imageSrc: "/images/profile-photos/ez-about.png",
 		linkedIn: "https://www.linkedin.com/in/ezekiel-lopez-633b57212/"
@@ -30,6 +35,11 @@ export const organizers = [
 		name: "Sam Holmes",
 		imageSrc: "/images/profile-photos/sam-2026.jpg",
 		linkedIn: "https://www.linkedin.com/in/sammyholmes/"
+	},
+	{
+		name: "Sonia Sarao",
+		imageSrc: "/images/profile-photos/sonia-2026.webp",
+		linkedIn: "https://www.linkedin.com/in/sonia-sarao/"
 	},
 	{
 		name: "Tryston Perry",
